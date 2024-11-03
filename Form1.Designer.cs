d6f4a0d514fd653a9b8586e2aac7c8efe9cd8492
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-11-03 17:18:54 +0700

    Main

---
+++ Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace gosLauncher
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.NotifyIcon notifyIcon; // NotifyIcon untuk tray
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip; // Menu konteks
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem; // Menu untuk keluar
+        private System.Windows.Forms.TextBox inputTextBox;
+
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SuspendLayout();
+            
+            // inputTextBox
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            // this.inputTextBox.BackColor = System.Drawing.Color.Lime;
+            this.inputTextBox.BackColor = System.Drawing.Color.Purple;
+            this.inputTextBox.ForeColor = System.Drawing.Color.White;
+            this.inputTextBox.Font = new System.Drawing.Font("FiraCode Nerd Font", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Size = new System.Drawing.Size(260, 25);
+            this.inputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputTextBox_KeyPress);
+            
+            // notifyIcon
+            this.notifyIcon.Icon = new System.Drawing.Icon(@"E:\Developer\Program\C#\gosLauncher\20230315_144251.ico");
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+
+            // contextMenuStrip
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitMenuItem});
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+
+            // Form1
+            this.KeyPreview = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // Tanpa title bar
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen; // Pusatkan form
+            this.BackColor = System.Drawing.Color.Lime; // Atur ke warna tertentu (akan digunakan untuk transparansi)
+            this.TransparencyKey = System.Drawing.Color.Lime; // Atur warna transparan
+            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.Controls.Add(this.inputTextBox);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace gosLauncher
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 9000;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Form1()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, HOTKEY_ID, 0x0002, (uint)Keys.Space);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                ShowForm();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void ShowForm()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+            inputTextBox.Focus();
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Control && e.KeyCode == Keys.W)
+            {
+                // Ambil teks saat ini dari inputTextBox
+                string currentText = inputTextBox.Text;
+
+                // Cari posisi terakhir dari spasi
+                int lastSpaceIndex = currentText.LastIndexOf(' ');
+
+                // Hapus kata terakhir
+                if (lastSpaceIndex >= 0)
+                {
+                    // Hapus kata terakhir dengan memotong string
+                    inputTextBox.Text = currentText.Substring(0, lastSpaceIndex);
+                }
+                else
+                {
+                    // Jika tidak ada spasi, hapus seluruh teks
+                    inputTextBox.Clear();
+                }
+
+                // Fokus kembali ke inputTextBox
+                inputTextBox.Focus();
+                e.SuppressKeyPress = true; // Menghindari bunyi beep saat menekan tombol
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                using (
+                    SoundPlayer player = new SoundPlayer(
+                        @"E:\Developer\Program\C#\gosLauncher\bye.wav"
+                    )
+                )
+                {
+                    player.Play();
+                }
+                this.Hide();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void inputTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // string inputText = inputTextBox.Text;
+                string inputText = inputTextBox.Text.Trim();
+                try
+                {
+                    var processInfo = new System.Diagnostics.ProcessStartInfo
+                    {
+                        FileName = inputText,
+                        UseShellExecute = true,
+                        WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                    };
+                    System.Diagnostics.Process.Start(processInfo);
+                    using (
+                        SoundPlayer player = new SoundPlayer(
+                            @"E:\Developer\Program\C#\gosLauncher\oke.wav"
+                        )
+                    )
+                    {
+                        player.Play();
+                    }
+                    inputTextBox.Clear();
+                    e.Handled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            ShowForm();
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                UnregisterHotKey(this.Handle, HOTKEY_ID);
+                components?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
